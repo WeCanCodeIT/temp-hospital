9cc72e938535a059bc338838545992ad788292f6
Alan <akostrick@hotmail.com>
2018-02-06 11:39:10 -0500

    Add Surgeon

---
+++ src/main/java/hospital/Surgeon.java
@@ -0,0 +1,16 @@
+package hospital;
+
+public class Surgeon extends Doctor {
+
+	private boolean isOperating;
+
+	public Surgeon(String empNumber, String empName, boolean isOperating) {
+		super(empNumber, empName);
+		this.isOperating = isOperating;
+	}
+
+	public boolean isOperating() {
+		return isOperating;
+	}
+
+}
